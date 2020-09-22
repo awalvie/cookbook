5fead400ec79a90553a8ef0760d38885081b09de
awalvie <agrawal.vishesh.178@gmail.com>
2020-09-22 10:57:07 +0530

    use renderer instead of surface

--- c/noodle/src/demo.c
+++ c/noodle/src/demo.c
@@ -2,68 +2,84 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
 
-#define SCREEN_WIDTH 640
-#define SCREEN_HEIGHT 480
-
-SDL_Window *gWindow = NULL;
-SDL_Renderer *renderer = NULL;
-SDL_Surface *gSurface = NULL;
-SDL_Surface *gCanvas = NULL;
+static int SCREEN_WIDTH = 640;
+static int SCREEN_HEIGHT = 480;
+uint32_t *pixels;
 bool gQuit = false;
 
-int error(char *msg, const char *err)
+SDL_Window *gWindow = NULL;
+SDL_Renderer *gRenderer = NULL;
+SDL_Texture *gTexture = NULL;
+
+void error(char *msg, const char *err)
 {
 	printf("Error %s: %s\n", msg, err);
 }
 
-bool init()
+int init_array()
 {
-	bool success = true;
-	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-		error("init", SDL_GetError());
-		success = false;
-	} else {
-		gWindow =
-			SDL_CreateWindow("noodle", SDL_WINDOWPOS_UNDEFINED,
-					 SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
-					 SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	int i, j;
+	pixels = (uint32_t *)malloc(SCREEN_WIDTH * SCREEN_HEIGHT *
+				    sizeof(uint32_t));
 
-		if (gWindow == NULL) {
-			error("window", SDL_GetError());
-			success = false;
-		} else {
-			gSurface = SDL_GetWindowSurface(gWindow);
-			if (gSurface == NULL) {
-				error("surface", SDL_GetError());
-				success = false;
-			}
+	if (pixels == NULL) {
+		error("init_array", "could not allocate memory for pixels");
+		return false;
+	}
+
+	for (i = 0; i < SCREEN_HEIGHT; i++) {
+		for (j = 0; j < SCREEN_WIDTH; j++) {
+			pixels[i * SCREEN_WIDTH +j] = 0xffffff;
 		}
 	}
 
-	return success;
+	return true;
 }
 
-bool loadMedia()
+bool init()
 {
-	bool success = true;
-
-	gCanvas = SDL_CreateRGBSurface(0, SCREEN_WIDTH, SCREEN_HEIGHT, 32, 0, 0,
-				       0, 0);
-	SDL_FillRect(gCanvas, NULL,
-		     SDL_MapRGB(gCanvas->format, 0xFF, 0xFF, 0xFF));
-
-	if (gCanvas == NULL) {
-		error("canvas", SDL_GetError());
-		success = false;
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		error("init", SDL_GetError());
+		return false;
 	}
 
-	return success;
+	gWindow = SDL_CreateWindow("noodle", SDL_WINDOWPOS_UNDEFINED,
+				   SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+				   SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+	if (gWindow == NULL) {
+		error("window", SDL_GetError());
+		return false;
+	}
+
+	gRenderer = SDL_CreateRenderer(gWindow, -1, 0);
+
+	if (gRenderer == NULL) {
+		error("renderer", SDL_GetError());
+		return false;
+	}
+
+	gTexture = SDL_CreateTexture(gRenderer, SDL_PIXELFORMAT_ARGB8888,
+				     SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH,
+				     SCREEN_HEIGHT);
+
+	if (gTexture == NULL) {
+		error("texture", SDL_GetError());
+		return false;
+	}
+
+	init_array();
+
+	return true;
 }
 
-bool quit()
+void quit()
 {
-	SDL_FreeSurface(gCanvas);
-	gCanvas = NULL;
+	free(pixels);
+	SDL_DestroyTexture(gTexture);
+	gTexture = NULL;
+	SDL_DestroyRenderer(gRenderer);
+	gRenderer = NULL;
 	SDL_DestroyWindow(gWindow);
 	gWindow = NULL;
 	SDL_Quit();
@@ -99,21 +115,24 @@ int main()
 {
 	if (!init()) {
 		fprintf(stderr, "ERROR: Could not initialize SDL\n");
-	} else {
-		SDL_BlitSurface(gCanvas, NULL, gSurface, NULL);
-		SDL_UpdateWindowSurface(gWindow);
-		/* main game loop */
-		SDL_Event event;
-
-		while (!gQuit) {
-			while (SDL_PollEvent(&event) != 0) {
-				if (event.type == SDL_MOUSEMOTION) {
-					handle_mouse(&event);
-				} else if (event.type == SDL_KEYDOWN) {
-					handle_keypress(&event);
-				}
+		quit();
+		return 0;
+	}
+	/* main game loop */
+	SDL_Event event;
+	while (!gQuit) {
+		SDL_UpdateTexture(gTexture, NULL, pixels,
+				  SCREEN_WIDTH * sizeof(uint32_t));
+		while (SDL_PollEvent(&event) != 0) {
+			if (event.type == SDL_MOUSEMOTION) {
+				handle_mouse(&event);
+			} else if (event.type == SDL_KEYDOWN) {
+				handle_keypress(&event);
 			}
 		}
+		SDL_RenderClear(gRenderer);
+		SDL_RenderCopy(gRenderer, gTexture, NULL, NULL);
+		SDL_RenderPresent(gRenderer);
 	}
 
 	quit();

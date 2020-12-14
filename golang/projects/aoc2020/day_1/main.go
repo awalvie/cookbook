9c9b9f186d47d67bc1d7b11557b8dd10b7f963f9
awalvie <agrawal.vishesh.178@gmail.com>
2020-12-14 13:31:35 +0530

    [golang/projects/aoc2020] add solution for part two of day 1

--- golang/projects/aoc2020/day_1/main.go
+++ golang/projects/aoc2020/day_1/main.go
@@ -43,7 +43,35 @@ func partOne(input []string) int {
 }
 
 // find the product of three numbers that add up to 2020
+func partTwo(input []string) int {
+
+	var multiplication int
+
+	for i := 0; i < len(input); i++ {
+		value_1, _ := strconv.Atoi(input[i])
+		for j := 0; j < len(input); j++ {
+			if i == j {
+				continue
+			}
+			value_2, _ := strconv.Atoi(input[j])
+			if value_1+value_2 < 2020 {
+				for k := 0; k < len(input); k++ {
+					if k == j || k == i {
+						continue
+					}
+					value_3, _ := strconv.Atoi(input[k])
+					if value_3+value_2+value_1 == 2020 {
+						multiplication = value_1 * value_2 * value_3
+						break
+					}
+				}
+			}
+		}
+	}
+
+	return multiplication
+}
 
 func main() {
-	fmt.Println(partOne(readInput("./input.txt")))
+	fmt.Println(partTwo(readInput("./input.txt")))
 }
